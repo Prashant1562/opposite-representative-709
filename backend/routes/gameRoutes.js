eb48075ddf3d77188af6dd0f38166cd1ddc9af7e
Govind Kumawat <kumawatgovind222@gmail.com>
2023-03-04 20:29:41 +0530

    added

--- backend/routes/gameRoutes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { playCard } from "../controllers/gameController";
-const router = Router();
-router.post("/play", playCard);
-export default router;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,14 @@
 const express=require('express')
-
+require('dotenv').config()
 const app=express()
 const http = require('http');
 const {Server} =require('socket.io')
-const cors=require('cors')
+const cors=require('cors');
+const { connection } = require('./config/db');
+const userRouter = require('./routes/userRoute');
 app.use(cors())
+app.use(express.json())
+app.use("/user",userRouter)
 const server=http.createServer(app)
 
 const io=new Server(server,{
@@ -27,6 +31,13 @@ io.on('connection',(socket)=>{
     })
 })
 
-server.listen(3004,()=>{
-    console.log(`server running at 3004`)
+server.listen(process.env.PORT,async()=>{
+    try {
+        await connection;
+        console.log("connected to db");
+      } catch (error) {
+        console.log(error);
+        res.sen({ message: "error while connecting to db" });
+      }
+      console.log(`running at http://localhost:${process.env.PORT}`);
 })
