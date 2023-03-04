e017d1184d88ea658fdc58599a8bfeaa135b9fdb
Govind Kumawat <kumawatgovind222@gmail.com>
2023-03-04 16:23:12 +0530

    added

---
+++ frontend/src/Routes/MainRoute.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Game from '../pages/Game'
+import Home from '../pages/home'
+import Leaderb from '../pages/Leaderb'
+
+const MainRoute = () => {
+  return (
+   <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/leader" element={<Leaderb/>} />
+    <Route path="/game" element={<Game/>} />
+   </Routes>
+  )
+}
+
+export default MainRoute
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./home.css"
+const Home = () => {
+    
+  return (
+<div className='homeMain'>
+  <input type="text" placeholder='Enter username' />
+  <button>Submit</button>
+</div>
+  )
+}
+
+export default Home
